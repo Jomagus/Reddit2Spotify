5e6e9364cbe26ddd6497c60213b5a0677eec0510
Jens Albert <j.albert@sepia.de>
2017-09-14 22:38:29 +0200

    Initial Commit

---
+++ dist/Reddit.js
@@ -0,0 +1,1 @@
+//# sourceMappingURL=Reddit.js.map
---
+++ test/test.js
@@ -0,0 +1,9 @@
+var expect = require('expect.js');
+
+describe("Reddit Tests", () => {
+
+    it("soll Beispiel sein", () => {
+        expect(1).to.be(1);
+    });
+
+});
